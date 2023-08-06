2aeb3972f70937148117211a5f57f208bcf5ba69
asfemi <pearlcstall@gmail.com>
2023-08-06 20:41:59 +0100

    started the quiz page on to the models and utilities

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lla/components/LLA_app_bar.dart';
+
+import '../components/LLA_bottom_navigational_bar.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -10,6 +13,84 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+//! the appbar
+     appBar: LLAAppBar('Q U I Z'),
+//!the body of the scaffold
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Container(
+              width: size.width * 0.9,
+              height: size.height * 0.35,
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  //keys,
+                  questions[totalQuestions].question,
+                  //questions[totalQuestions].toString(),
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            SizedBox(
+              height: size.height * 0.45,
+              child: ListView.builder(
+                itemCount: options.length - 1,
+                // itemExtent: options.length.toDouble() - 1,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    //TODO: make this buttons white
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        bool allQuestionsAnswered = await checkAnswer(
+                            options[totalQuestions].option[index],
+                            questions[totalQuestions].answer);
+
+                        if (allQuestionsAnswered) {
+                          navigateToResultPage();
+                        }
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(4)),
+                          textStyle: MaterialStateProperty.all(
+                              const TextStyle(fontSize: 20))),
+                      child: SizedBox(
+                        width: size.width * 0.7,
+                        height: size.height * 0.1,
+                        child: Center(
+                          child: Text(
+                            options[totalQuestions].option[index],
+                            style: const TextStyle(
+                                fontSize: 18, color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          
+
+        ],),
+//! the bottom navigational bar
+      bottomNavigationBar: LLABottomNavigationBar(context),
+    
+    );
   }
 }
